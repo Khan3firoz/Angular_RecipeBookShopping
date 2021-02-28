1f9ad124732a49688e4124c0aff54fba36bbfeb8
Firoz <khan3firoz@gmail.com>
2021-02-28 11:24:29 +0530

    added dropdown functionality and manage all component accordingto th project structure

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { from } from 'rxjs';
 import {Recipes} from '../recipes.model'
 @Component({
@@ -7,12 +7,18 @@ import {Recipes} from '../recipes.model'
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipes>();
   recipes: Recipes[] = [
+    new Recipes('A Test Recipe', 'This is simply a test','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
     new Recipes('A Test Recipe', 'This is simply a test','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
   ];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe:Recipes) {
+    this.recipeWasSelected.emit(recipe)
   }
 
 }
