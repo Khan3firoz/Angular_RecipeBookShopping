2ee505242b5791eb393234d15d32526b33401533
Firoz <khan3firoz@gmail.com>
2021-03-01 12:22:54 +0530

    added routing and componets added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shopingBook';
   loadedFeature = 'recipe';
-  onNavigate(feature:string) {
+
+  onNavigate(feature: string) {
     this.loadedFeature = feature;
   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,8 @@
-import { Component, OnInit,EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  templateUrl: './header.component.html'
 })
-export class HeaderComponent  {
-  @Output() featureSlected = new EventEmitter<string>();
-  onSlect(feature:string) {
-    this.featureSlected.emit(feature)
-  }
-
+export class HeaderComponent {
 }
